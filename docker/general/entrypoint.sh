6c781b6af87fbe7d67a55ee9ccf291995c7eb4ea
ariesArch <kyawyenaing.dev@gmail.com>
2023-11-17 10:37:53 +0630

    fix entrypont format

--- docker/general/entrypoint.sh
+++ docker/general/entrypoint.sh
@@ -1,19 +1,5 @@
-# #!/bin/bash
-# if [ -z "$APP_ENV" ]; then
-#     echo "APP_ENV is not set. Please set the APP_ENV environment variable."
-#     # exit 1
-# fi
+#!/bin/bash
 
-# if [ "$APP_ENV" = "dev" ]; then
-#     ENV_FILE=".env.dev"
-# fi
-
-# if [ ! -f ".env" ]; then
-#     echo "Creating env file for env $APP_ENV"
-#     cp "$ENV_FILE"  .env
-# else
-#     echo "env file exists."
-# fi
 npm install
 npm run build
 php artisan migrate
